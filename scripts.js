5f655fd0cab3da7ac9e21ea6ea6404fbbe3902c7
viivigood <vivian0306@hotmail.com>
2019-01-26 13:09:19 -0300

    formulary

--- scripts.js
+++ scripts.js
@@ -45,3 +45,69 @@ function numberOnly(id) {
     var regex = /[^0-9]/gi;
     element.value = element.value.replace(regex, "");
 }
+
+
+
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+
+
+
+  jQuery.validator.addMethod("isValidEmail", function(email) {
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    return regex.test(email); 
+  }, "Email no valido");
+
+
+  $('#formRegister').validate({
+    rules: {
+      phone : { number: true, minlength: 9 },
+      name : {},
+      email : {isValidEmail: true },
+      message : {}
+    },
+    messages: {
+      name : { 
+        required: 'Un nombre es requerido'},
+      phone : { 
+        required: 'Un número es requerido',
+        number: 'Debe ser un número',
+        minlength: 'Debe contener al menos 9 digitos' },
+      email : {
+        required: 'Un email es requerido'},
+      message : {
+        required : "Un mensaje es requerido"
+      }
+    },
+    errorClass: "error",
+    validClass: "valid-feedback",
+    errorElement: "div",
+    submitHandler: function(form){
+      var options = {
+        target: '',
+        beforeSubmit: function(){ console.log('submitted'); },
+        success: function() { 
+          console.log('success');
+          //@todo: crear una alerta que muestre que el formulario se ha enviado correctamente
+        }
+      };
+
+      $(form).ajaxSubmit(options); 
+    }
+  });
+
+  })();
